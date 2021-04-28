882bee4ed79c3a32802ea311032c11907dcec182
BuseglY <busegly@outlook.com>
2021-04-28 03:03:04 +0300

    (Java & React ) Yazılım Geliştirici Yetiştirme Kampı

---
+++ intro/src/intro/Main.java
@@ -0,0 +1,73 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//camelCase
+		//Don't repeat yourself.
+		String internetSubeButonu="Ýnternet Þubeye gir";
+		System.out.println(internetSubeButonu);
+		double dolarDun=8.20;
+		double dolarBugun=8.20;
+		if(dolarBugun<dolarDun) {
+			System.out.println("Dolar düþtü resmi");
+		}
+		else if(dolarBugun>dolarDun) {
+			System.out.println("Dolar yükseldi resmi");
+		}
+		else {
+			System.out.println("Dolar eþittir resmi");
+		}
+
+		String kredi1="Hýzlý Kredi";
+		String kredi2="Mutlu Emekli Kredisi";
+		String kredi3="Konut Kredisi";
+		String kredi4="Çiftçi Kredisi";
+		String kredi5="MSB Kredisi";
+		String kredi6="MEB Kredisi";
+		String kredi7="Kültür Bakanlýðý Kredisi";
+		
+		System.out.println(kredi1);
+		System.out.println(kredi2);
+		System.out.println(kredi3);
+		System.out.println(kredi4);
+		System.out.println(kredi5);
+		System.out.println(kredi6);
+		System.out.println(kredi7);
+		
+		String [] krediler= {"Hýzlý Kredi","Mutlu Emekli Kredisi",
+				"Konut Kredisi","Çiftçi Kredisi","MSB Kredisi",
+				"MEB Kredisi","Kültür Bakanlýðý Kredisi"};
+		
+		//foreach döngüsü
+		for(String kredi:krediler) {
+			System.out.println(kredi);
+		}
+		//for kullanýmý
+		for(int i=0;i<krediler.length;i++) {
+			System.out.println(krediler[i]);
+		}
+		//Deðer Tipler
+		int sayi1=10;
+		int sayi2=20;
+		sayi1=sayi2;
+		sayi2=100;
+		System.out.println(sayi1);//20
+		//Referans Tipler
+		int[]sayilar1= {1,2,3,4,5};
+		int[] sayilar2= {10,20,30,40,50};
+		sayilar1=sayilar2;//sayilar1 dizisinin referans numarasý 
+		//artýk sayilar2 dizisinin referans numarasý oldu.
+		sayilar2[0]=100;
+		System.out.println(sayilar1[0]);//100
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
